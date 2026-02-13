24f6002700686b90e3e56d02aebf7758194f252c
acreegan <andrew.s.creegan@gmail.com>
2026-02-13 15:11:47 +1100

    update astro content collections and fix preview image displaying on arrow card

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,9 @@
-import { defineCollection, z} from "astro:content";
 import { glob } from "astro/loaders";
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
 
 const blog = defineCollection({
-  type: "content",
-  // loader: glob({ pattern: '**/[^_]*.md', base: "./src/content/blog" }),
+  loader: glob({ pattern: '**/[^_]*.md', base: "./src/content/blog" }),
   schema: ({image}) => z.object({
     title: z.string(),
     description: z.string(),
